54756a1a4965bbbca1a196dcac85875a2e4c7ad9
Student Laptop User <fuksi@dhcp-asv-103.helsinki.fi>
2016-09-08 17:06:31 +0300

    ekan viikon palautus

---
+++ FlashRep/src/main/java/flashrep/flashrep/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flashrep.flashrep;
+
+/**
+ *
+ * @author Raine Rantanen
+ */
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}

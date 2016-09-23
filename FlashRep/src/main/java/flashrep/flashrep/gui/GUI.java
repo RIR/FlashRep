c8203a2208374d123b748f5d969b0fb87653bb6a
Student Laptop User <fuksi@dhcp-asv-103.helsinki.fi>
2016-09-23 11:42:20 +0300

    tuntikirjanpitoa päivitetty

---
+++ FlashRep/src/main/java/flashrep/flashrep/gui/GUI.java
@@ -0,0 +1,28 @@
+package flashrep.flashrep.gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Raine Rantanen
+ */
+public class GUI implements Runnable {
+
+    private JFrame frame;
+
+    public GUI() {
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void initComponents (){
+        
+    }
+    
+    public JFrame getFrame() {
+        return frame;
+    }
+}
--- FlashRep/src/main/java/flashrep/flashrep/logic/SpacedRepetition.java
+++ FlashRep/src/main/java/flashrep/flashrep/logic/SpacedRepetition.java
@@ -2,7 +2,6 @@ package flashrep.flashrep.logic;
 
 import flashrep.flashrep.cards.Flashcard;
 import flashrep.flashrep.cards.FlashcardCollection;
-import java.util.ArrayDeque;
 import java.util.PriorityQueue;
 import java.util.Queue;
 

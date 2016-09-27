d77e973dc318e934bdec09be30f7c385c56b01c5
Student Laptop User <fuksi@dhcp-asv-103.helsinki.fi>
2016-09-27 22:16:27 +0300

    JavaDocin lisäilyä

--- FlashRep/src/main/java/flashrep/flashrep/logic/SpacedRepetition.java
+++ FlashRep/src/main/java/flashrep/flashrep/logic/SpacedRepetition.java
@@ -10,18 +10,33 @@ import java.util.Queue;
 /**
  * @author Raine Rantanen
  */
+/**
+ * Luokka korttikokoelmien aikavälikertaukseen, toteuttaa
+ * RepetitionLogic-rajapinnan.
+ */
 public class SpacedRepetition implements RepetitionLogic {
 
     private PriorityQueue<Flashcard> rotationQueue;
     private boolean removeFromRotation;
     private FlashcardCollection flashcardCollection;
 
+    /**
+     * Luokan konstruktori, asettaa parametrina annettavan korttikokoelman
+     * toistojonoon.
+     *
+     * @param flashcardCollection Parametrina annettava korttikokoelma
+     */
     public SpacedRepetition(FlashcardCollection flashcardCollection) {
         this.rotationQueue = new PriorityQueue<Flashcard>();
         this.loadFlashcardCollectionIntoRotation(flashcardCollection);
         removeFromRotation = false;
     }
 
+    /**
+     * Metodi lataa parametrina annettavan korttikokoelman toistojonoon.
+     *
+     * @param flashcardCollection Parametrina annettava toistojono
+     */
     public void loadFlashcardCollectionIntoRotation(FlashcardCollection flashcardCollection) {
         this.flashcardCollection = flashcardCollection;
         if (!this.rotationQueue.isEmpty()) {
@@ -30,6 +45,12 @@ public class SpacedRepetition implements RepetitionLogic {
         this.rotationQueue.addAll(this.flashcardCollection.getCards());
     }
 
+    /**
+     * Metodi palauttaa toistojonossa ensimmäisenä olevan kortin, tai null jos
+     * jono on tyhjä.
+     *
+     * @return toistojonon ensimmäinen kortti tai null jos jono on tyhjä
+     */
     public Flashcard showCard() {
         this.removeFromRotation = false;
         if (!this.rotationQueue.isEmpty()) {
@@ -38,24 +59,44 @@ public class SpacedRepetition implements RepetitionLogic {
         return null;
     }
 
+    //Pitäisi ehkä olla yksityinen metodi? 
+    /**
+     * Metodi asettaa parametrina annettavan kortin toistojonoon, jos ei ole
+     * valittu, että kortti poistetaan toistojonosta.
+     *
+     * @param flashcard Parametrina annettava kortti
+     */
     public void insertCardInToRotation(Flashcard flashcard) {
         if (!isRemovedFromRotation()) {
             this.rotationQueue.add(flashcard);
         }
     }
 
+    //Yksityinen metodi ei JavaDociin
     private boolean isRemovedFromRotation() {
         return removeFromRotation;
     }
 
+    /**
+     * Metodi poistaa metodia käyttävän kortin toistojonosta.
+     */
     public void removeFromRotation() {
         this.removeFromRotation = true;
     }
 
+    /**
+     * Metodi poistaa kaikki kortit toistojonosta.
+     */
     public void removeAllCardsFromRotation() {
         this.rotationQueue.clear();
     }
 
+    /**
+     * Metodi palauttaa toistojonon FlashcardCollection-muodossa.
+     *
+     * @return toistojonon kortit tallennettuna
+     * FlashcardCollection-korttikokoelmaan
+     */
     public FlashcardCollection getFlashcardCollection() {
         this.flashcardCollection.removeAllCardsFromCollection();
         this.flashcardCollection.getCards().addAll(rotationQueue);
